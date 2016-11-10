7df8c514351c99eadabba2cb299c0337b0a3ab02
Malachi Gray <malachigray@C02RC3QSG8WM.local>
2016-11-10 16:10:28 -0500

    l13 displaying the single photo component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments'
+
 
 class Single extends React.Component {
 
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+
+    // get us the post
     return (
       <div className='single-photo'>
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
